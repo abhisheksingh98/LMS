4c1b6509c18566621d17506f35fcd61cade86cd7
abhisheksingh98 <writetoabhishek07@gmail.com>
2023-08-24 02:11:38 +0530

    Add Register api

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,5 +1,18 @@
 import express from "express";
+import { register } from "../controllers/userController.js";
 
 const router = express.Router();
+
+//to register a new user
+router.route("/register").post(register);
+
+//login
+//logout
+
+//get my profile
+
+//change password
+
+//forgot password
 
 export default router;
